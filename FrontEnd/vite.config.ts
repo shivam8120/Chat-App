e02c67de66e383d3300f7a6ccb54ffda49b79544
Shivam singh <147019658+shivam8120@users.noreply.github.com>
2024-09-18 18:14:34 +0530

    Update vite.config.ts

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server : {
     proxy : {
       "/api" : {
-        target : "https://chat-app-4mff.vercel.app",
+        target : "http://localhost:3000",
       }
     }
   }
